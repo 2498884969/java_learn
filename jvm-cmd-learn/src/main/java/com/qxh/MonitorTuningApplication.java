699edee96199b9568769b3dac43d78f750ae75df
2498884969 <13814032052@163.com>
2020-05-10 10:40:26 +0800

    jvm 调优

---
+++ jvm-cmd-learn/src/main/java/com/qxh/MonitorTuningApplication.java
@@ -0,0 +1,14 @@
+package com.qxh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorTuningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorTuningApplication.class,args);
+    }
+
+
+}
